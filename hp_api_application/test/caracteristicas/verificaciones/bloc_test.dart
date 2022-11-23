89d3a34c806dfb935992309b543d8ab386cb2990
brandonruiz123 <L18440503@mochis.tecnm.mx>
2022-11-22 21:39:40 -0700

    Crea primer test para validar el estado emitido al agregar el evento Creado

---
+++ hp_api_application/test/caracteristicas/verificaciones/bloc_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hp_api_application/caracteristicas/verificaciones/bloc.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+void main() {
+  blocTest<BlocVerificacion, Estado>(
+    'emite el estado "PrimeraPantalla" cuando se agrega el evento "Creado"',
+    build: () => BlocVerificacion(),
+    act: (bloc) => bloc.add(Creado()),
+    expect: () => [isA<PrimeraPantalla>()],
+  );
+}
